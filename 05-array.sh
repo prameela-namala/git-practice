c1f3a644531b24fb4b288a83369aa1517a15fb3b
prameela <prameelanamala.com>
2024-08-26 15:10:56 +0530

    reading password

--- 05-array.sh
+++ 05-array.sh
@@ -3,11 +3,11 @@
 
  FRUITS=("APPLE" "KIWI" "PAPAYA")
 
- echo "first fruit is: $(FRUITS(0))"
+ echo "first fruit is: ${FRUITS(0)}"
 
 
- echo "second fruit is: $(FRUITS(1))"
+ echo "second fruit is: ${FRUITS(1)}"
 
- echo "third fruit is: $(FRUITS(2))"
+ echo "third fruit is: ${FRUITS(2)}"
 
- echo "all fruits are: $(FRUITS(@))"
+ echo "all fruits are: ${FRUITS(@)}"
