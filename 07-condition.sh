e64be2c4483dc2b72a21e49c0399c03fe5cc17b2
prameela <prameelanamala.com>
2024-08-26 22:51:03 +0530

    sum

---
+++ 07-condition.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+NUMBER=$1
+if[$number -gt 20]
+
+then
+echo "given number:$number greter than 20"
+
+else
+echo "given number: $number is less than 20"
+fi
--- 07.condition.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-DATE=$(date)
-
-echo $DATE 
