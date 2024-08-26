5d1909d163e9dc9d099a3ad48ebf2ae2ccb37ee4
prameela <prameelanamala.com>
2024-08-26 22:58:20 +0530

    sum

--- 07-condition.sh
+++ 07-condition.sh
@@ -5,13 +5,11 @@ NUMBER=$1
 if[ $number -gt 20 ];
 
 
-{
-echo "given number:$number greter than 20"
+
+echo "given number:$number greter than 20";
 
 else
 
-{
-echo "given number: $number is less than 20"
-}
-}
+echo "given number: $number is less than 20";
+
 fi
