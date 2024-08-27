503cbcb2d3bbf4b905bfa36e82851425d617c2dc
prameela <prameelanamala.com>
2024-08-27 22:45:00 +0530

    sum

--- 13-loops.sh
+++ 13-loops.sh
@@ -1,49 +1,40 @@
 #!/bin/bash
 
 USERID=$(id -u)
-
 R="\e[31m"
-G="\e[32m
-Y="\e[33m"
+G="\e[32m"
 N="\e[0m"
 
-
-CHECK-ROOT()
-{
-if [ $USERID -ne 0 ]
-echo  "please run with root prveliages"
-exit 1
-fi
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo "Please run this script with root priveleges"
+        exit 1
+    fi
 }
 
-VALIDATE()
-{
- if [ $1 -ne 0 ]
- then 
-    echo "$2 is ...$R failed $N "
-   exit 1
-  
-   else
-         echo "$2 is....$G success $N "
-fi
-
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 is...$R FAILED $N"
+        exit 1
+    else
+        echo -e "$2 is... $G SUCCESS $N"
+    fi
 }
 
 CHECK_ROOT
 
-for  package in $@
+# sh 15-loops.sh git mysql postfix nginx
+for package in $@ # $@ refers to all arguments passed to it
 do
-   dnf list installed $package
-
-if [ $? -ne 0 ]
-
-then
-    echo "$package is not installed,going to install it.."
-
-    dnf install $package -y
-    VALIDATE $? "installing package"
-else
-  echo " $package already installed "
-fi
- 
+    dnf list installed $package
+    if [ $? -ne 0 ]
+    then
+        echo "$package is not installed, going to install it.."
+        dnf install $package -y
+        VALIDATE $? "Installing $package"
+    else
+        echo "$package is already installed..nothing to do"
+    fi
 done
