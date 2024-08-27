4b4913ce5944701090942886dbb7943fc78e1aa1
prameela <prameelanamala.com>
2024-08-27 15:35:58 +0530

    sum

--- 11-sqlinstallation.sh
+++ 11-sqlinstallation.sh
@@ -1,47 +1,22 @@
-
-
+#!/bin/bash
 #!/bin/bash
 
+
 USERID=$(id -u)
+echo "userid is :$USERID"
 
-CHECK_ROOT(){
-    if [ $USERID -ne 0 ]
-    then
-        echo "Please run this script with root priveleges"
-        exit 1
-    fi
-}
-
-VALIDATE(){
-    if [ $1 -ne 0 ]
-    then
-        echo "$2 is...FAILED"
-        exit 1
-    else
-        echo "$2 is...SUCCESS"
-    fi
-}
-
-CHECK_ROOT
-
-dnf list installed git
-
-if [ $? -ne 0 ]
-then
-    echo "Git is not installed, going to install it.."
-    dnf install git -y
-    VALIDATE $? "Installing Git"
-else
-    echo "Git is already installed, nothing to do.."
+if [ $USERID -ne 0 ]
+then 
+echo "please run this script with root prevelages"
 fi
 
-dnf list installed mysql
+dnf install git -y
 
+dnf list installed git
 if [ $? -ne 0 ]
 then
-    echo "MySQL is not installed...going to install"
-    dnf install mysql -y
-    VALIDATE $? "Installing MySQL"
+echo "git is not installed ,we are going to install"
 else
-    echo "MySQL is already installed..nothing to do"
+dnf install git -y
+echo  "git is already installed,nothing to do"
 fi
