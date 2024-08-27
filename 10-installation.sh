fa8309f300285e182019998c8a797ef961e9d6c1
prameela <prameelanamala.com>
2024-08-27 13:30:37 +0530

    sum

---
+++ 10-installation.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+USERID=$(id -u)
+echo "userid is :$USERID"
+
+if [ $USERID -ne 0 ]
+then 
+echo "please run this script with root prevelages"
+ exit 1
+fi
+dnf list installed git
+if [ $? -ne 0 ]
+then
+echo "git is not installed ,we are going to install"
+dnf install -y
+else
+echo  "git is already installed,nothing to do"
