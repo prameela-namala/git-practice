302e295c6382e95d257d9d55455fc0716bb936e5
prameela <prameelanamala.com>
2024-08-27 13:40:49 +0530

    sum

--- 10-installation.sh
+++ 10-installation.sh
@@ -12,7 +12,7 @@ dnf list installed git
 if [ $? -ne 0 ]
 then
 echo "git is not installed ,we are going to install"
-dnf install git -y
 else
+dnf install git -y
 echo  "git is already installed,nothing to do"
 fi
