c2453c3019d96894ad00260be2c7c4dd013988b4
prameela <prameelanamala.com>
2024-08-28 13:56:19 +0530

    sum

--- 14-redirections.sh
+++ 14-redirections.sh
@@ -45,7 +45,7 @@ do
     then
         echo "$package is not installed, going to install it.."&>>$LOG_FILE 
         dnf install $package -y &>>$LOG_FILE
-        VALIDATE $? "Installing $package"&>>$LOG_FILE
+        VALIDATE $? "Installing $package"
     else
         echo -e " $package $Y is already installed..nothing to do $N " | tee -a $LOG_FILE
     fi
