47c2da5a18cf5b41bb3c1609523cede533a0d223
prameela <prameelanamala.com>
2024-08-28 13:41:04 +0530

    sum

--- 14-redirections.sh
+++ 14-redirections.sh
@@ -2,7 +2,8 @@
 LOG_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
-LOG-FILE="$LOG_FOLDER/$SCRIPT_NAME-TIMESTAMP.log"
+LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME-TIMESTAMP.log"
+
 mkdir -p $LOG_FOLDER
 
 
