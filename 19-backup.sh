6ce77df24dfd85032256511603a0504b724acedb
prameela <prameelanamala.com>
2024-08-31 16:28:16 +0530

    installing mysql

--- 19-backup.sh
+++ 19-backup.sh
@@ -2,44 +2,59 @@
 
 SOURCE_DIR=$1
 DEST_DIR=$2
-DAYS=${3: -14}
+DAYS=${3:-14} #if $3 is empty, default is 14 days.
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
 USAGE(){
-    echo "USAGE::sh 19.backup.sh <sourcefile> <destination> <days(optional)>
+    echo -e "$R USAGE:: $N sh 19-backup.sh <source> <destination> <days(optional)>"
 }
-if [$# -lt 2 ]
-then  
-  USAGE
-  exit 1
-fi
+#check the source and destination are provided
 
-if [! -d $SOURCE_DIR ]
-then 
-echo "$SOURCE_DIR does not exist...please check"
-fi
-if [ ! -d $DEST_DIR ]
-then 
- echo "$DEST_DIR does not exist...please check"
-fi
-FILES=$(find ${SOURCE_DIR} -NAME "*.log" -mtime +14)
-if [ -z $FILES ]
+if [ $# -lt 2 ]
 then
- echo "files are found "
- ZIP_FILE="$DEST_DIR/app-logs-$TIMESTAMP.zip"
- find ${SOURCE_DIR} -name "*.log" -mtime +14 |zip "$ZIP_FILE" -@
-
-   if [ -f $ZIP_FILE ]
-      then
-        echo "successfully zipped files older than $DAYS"
-        while IFS= read -r
-     do
-   echo "deleting file:$file"
-   rm -rf $file
-   done <<< $FILES
-   else 
-     echo "zipping the file is failed"
+    USAGE
     exit 1
-    fi  
+fi
+
+if [ ! -d $SOURCE_DIR ]
+then
+    echo "$SOURCE_DIR does not exist...Please check"
+fi
+
+if [ ! -d $DEST_DIR ]
+then
+    echo "$DEST_DIR does not exist...Please check"
+fi
+
+FILES=$(find ${SOURCE_DIR} -name "*.log" -mtime +14)
+
+echo "Files: $FILES"
+
+if [ ! -z $FILES ] #true if FILES is empty, ! nakes it expression false
+then
+    echo "Files are found"
+    ZIP_FILE="$DEST_DIR/app-logs-$TIMESTAMP.zip"
+    find ${SOURCE_DIR} -name "*.log" -mtime +14 | zip "$ZIP_FILE" -@
+
+    #check if zip file is successfully created or not
+    if [ -f $ZIP_FILE ]
+    then
+        echo "Successfully zippped files older than $DAYS"
+        #remove the files after zipping
+        while IFS= read -r file #IFS,internal field seperatpor, empty it will ignore while space.-r is for not to ingore special charecters like /
+        do
+            echo "Deleting file: $file"
+            rm -rf $file
+        done <<< $FILES
+    else
+        echo "Zipping the files is failed"
+        exit 1
+    fi
 else
-   echo "nofiles older than $DAYS"
+    echo "No files older than $DAYS"
 fi
