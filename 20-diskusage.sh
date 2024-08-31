9108104a1ad487eb65e57b7dc06dd094fc927dbb
prameela <prameelanamala.com>
2024-08-31 18:41:27 +0530

    installing mysql

---
+++ 20-diskusage.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+DISK_USAGE=$(df -hT | grep xfs)
+DISK_THRESHOLD=5
+while IFS= read -r line
+do 
+USAGE=$(echo $line  df -hT | grep xfs |awk -F " " '{print $6F}' |cut -d "%" -f1)
+    PARTITION=$(echo $line  df -hT | grep xfs |awk -F " " '{print $NF}')
+    if [ $USAGE -ge $disk_threshold ]
+    then 
+      echo "$PARTITION is more than $THRESHOLD:current value is $USAGE"
+    done <<< $DISK_USAGE   
