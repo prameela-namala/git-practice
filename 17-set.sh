93d6a6027cfbfbca8e12749301d7d3e55ed460f5
prameela <prameelanamala.com>
2024-08-31 15:29:52 +0530

    installing mysql

--- 17-set.sh
+++ 17-set.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 set -e 
+ failure(){
 
+    echo "failed at:$1 and $2"
+
+ }
+trap ' failure "${LINENO}" "$BASH_COMMAND" ' ERR
 echo "hello world"
 echooo "hello world"
 echo "helloworld two"
