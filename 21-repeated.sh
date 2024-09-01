b95e84e4e379903e47f109ac02145fac5139aa72
prameela <prameelanamala.com>
2024-09-01 11:49:17 +0530

    installing mysql

--- 21-repeated.sh
+++ 21-repeated.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 FILE_NAME=$1
+uniq -c=$2
 if [ $FILE_NAME ]
 then
-cat $FILE_NAME | awk -F " " '{print $NF}'
+cat $FILE_NAME | $2 $FILE_NAME
+echo " the words and their count i a file: $2"
 fi
