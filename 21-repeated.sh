a36b994a2466e91198e4c03278d2ba2db2b5bf62
prameela <prameelanamala.com>
2024-09-02 11:50:46 +0530

    occurance

--- 21-repeated.sh
+++ 21-repeated.sh
@@ -4,7 +4,7 @@ FILE_NAME=$1
  if [ $FILE_NAME ]
  then 
    echo "content in the file before replace :: cat $FILE_NAME"
-   echo "content in the file after replace : : cat $FILE_NAME | sed "s/find/replace/g"
+   echo "content in the file after replace : : cat $FILE_NAME | sed "s/find/replace/g" "
 fi
 
 
